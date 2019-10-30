0e8098b5e1030abcade8d45a807d31b2266e3700
swiftserieschannel <48859783+swiftserieschannel@users.noreply.github.com>
2019-10-30 13:30:53 +0530

    Customization in YPImagePicker
    Customization in YPImagePicker

---
+++ YPCustomizationTutorial/NewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewViewController.swift
+//  YPCustomizationTutorial
+//
+//  Created by Chander Bhushan on 30/10/19.
+//  Copyright © 2019 Swift Series. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- YPCustomizationTutorial/ViewController.swift
+++ YPCustomizationTutorial/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var config = YPImagePickerConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        config.onlySquareImagesFromCamera = true
+        config.usesFrontCamera = false
+        config.showsPhotoFilters = true
+        config.shouldSaveNewPicturesToAlbum = true
+        config.albumName = "DefaultYPImagePickerAlbumName"
+        config.startOnScreen = YPPickerScreen.photo
+        config.screens = [.library, .photo, .video, .newVC]
+        config.showsCrop = .none
+        config.targetImageSize = YPImageSize.original
+        config.overlayView = UIView()
+        config.hidesStatusBar = true
+        config.hidesBottomBar = false
+        config.preferredStatusBarStyle = UIStatusBarStyle.default
+        config.bottomMenuItemSelectedColour = UIColor(r: 38, g: 38, b: 38)
+        config.bottomMenuItemUnSelectedColour = UIColor(r: 153, g: 153, b: 153)
     }
-
-
+    
+    
+    @IBAction func clickedBtnShowImagePicker(_ sender: Any) {
+        
+        
+        
+        let picker = YPImagePicker(configuration: config)
+        
+        
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
